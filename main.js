1950fce7d21dc5c187e4d1918a2d1cd872d1aec3
Fishir123 <faiqthestrongest@gmail.com>
2023-11-04 08:11:13 +0700

    menambahkan index.html & main.js

---
+++ main.js
@@ -0,0 +1,46 @@
+// alert('heyho!')
+
+// let usia = 30
+// usia = 35
+// console.log('heyhoo usia kamu adalah ' + usia )
+// alert('usia kamu adalah ' + usia)
+
+// let usia = prompt('berapa usia kamu?')
+// alert('usia anda adalah ' + usia)
+
+let nama = 'Faiq Muntashir' //tipe string
+let usia = 17 // tipe integer number
+let tinggiBadan = 173.5 //tipe double float
+let beratBadan 
+let pacar = null 
+
+beratBadan = 75 //dipangill variable beratBadan 
+
+// if(pacar == null){ //condition 
+//     pacar = 'belum punya'
+// }else{
+//     pacar = 'udah punya'
+// }
+
+switch(pacar) {
+    case 1:
+        pacar = 'punya 1 aja'
+    break
+    case 2:
+        pacar = 'punya pacar 2, aku cukup playboy'
+    break
+    default:
+        pacar = 'belum punya pacar'
+        break 
+    }
+
+let saldoAwal = 50000
+let saldoTambahan = 8000
+
+// alert('nama anda adalah ' + nama + ' dan usia anda adalah ' + usia)
+
+alert(
+    `nama saya ${nama} usia saya itu ${usia} tinggi badan saya adalah ${tinggiBadan}cm berat badan saya ${beratBadan}kg dan pacar saya ${pacar}`,
+)
+
+alert(`saldo awal saya sebesar ${saldoAwal} & saldo tambahan yang akan saya miliki sebesar ${saldoTambahan}`) 
